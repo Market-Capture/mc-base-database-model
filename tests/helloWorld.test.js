22a932d529065d7baa39e1cd603396b794d0218e
taylortreece <taylor@taylortreece.com>
2023-03-12 20:43:37 -0500

    modified helloWorldInstance test

--- tests/helloWorld.test.js
+++ tests/helloWorld.test.js
@@ -2,8 +2,9 @@ import { helloWorldInstance } from "../src/index.js";
 
 describe("Hello World Test", () => {
   it("should say hello from the file name from which it was instantiated.", () => {
-    expect(helloWorldInstance.sayHello()).toBe(
-      "hello from helloWorldModel.js!"
-    );
+    const fileName = helloWorldInstance.fileName;
+    const say = helloWorldInstance.sayHello();
+
+    expect(say).toBe(`hello from ${fileName}!`);
   });
 });
